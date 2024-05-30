c9cda12783ba23503abb64ac164f49c85fb24ae2
Herianto <52534945+heriantosy@users.noreply.github.com>
2024-05-30 23:10:25 +0700

    x

--- database/migrations/2024_05_30_121209_create_table_hari.php
+++ database/migrations/2024_05_30_121209_create_table_hari.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('hari', function (Blueprint $table) {
          $table->increments('HariID');
          $table->string('NamaHari');
-         $table->string('img');
+         $table->string('img')->default('default.jpg');
         });
     }
 
